711fd7108734d38bb37a7526fd673f878c4b4683
Josh Rudesill <joshrudesill@gmail.com>
2024-02-05 14:27:27 -0600

    env

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV === "test") {
 }
 
 const pool = new pg.Pool({
-  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+  connectionString: process.env.POSTGRES_URL_URL + "?sslmode=require",
 });
 
 module.exports = pool;
